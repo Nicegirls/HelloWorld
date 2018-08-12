83bba65c02d72f7a9fbdc346b357ba338b80e504
child <835202945@qq.com>
2018-08-12 15:30:04 +0800

    add some test

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,57 @@
+import java.util.ArrayList;
 
 public class HelloWorld {
 	public static void main(String[]args){
-		System.out.println("Hello World! Ëæ±ã¸Ä¸Ä");
+//		int a = 0;
+//		int c = 0;
+//		do{
+//			--c;
+//			a=a-1;
+//		}while(a>0);
+//		System.out.println(c);
+//		int []a = new int[3];
+//		int i = a.length;
+//		int a = 1;
+//		int b =10;
+//		do{
+//			b-=a;
+//			a++;
+//		}while(b--<0);
+//		System.out.println(b);
+		System.out.println();
+		Test t = new Test();
+//		System.out.println(t.f());
+		System.out.println(t.getValue(2));
+		ArrayList al = new ArrayList();
+		long temp = (int)3.9;
+		temp%=2;
+		System.out.println(temp);
+		int a = 10;
+		int m = 5;
+		m = ++a*m;
+		System.out.println(m);
+	}
+}
+class Test extends Thread{
+	public int f(){
+		int i = 0;
+		try{
+			++i;
+		}finally{
+			++i;
+		}
+		return ++i;
+	}
+	public static int getValue(int i){
+		int result = 0;
+		switch(i){
+		case 1:
+			result = result+i;
+		case 2:
+			result = result+i+2;
+		case 3:
+			result = result+i+3;
+		}
+		return result;
 	}
 }
